fa8b2871a59f156c7dd684d343466618fd70680b
kagoesy <11874265@qq.com>
2019-11-25 23:09:44 +0800

    add select sort

--- sortalg.py
+++ sortalg.py
@@ -1,5 +1,26 @@
-class my_class(object):
-    pass
+class sortAlgorithm(object):
+
+    def choosesort(self,x):
+        N=len(x)
+        for i in range(len(x)):
+            min=i
+            for j in range(i,len(x)):
+                if less(x[j],x[min]):
+                    min=j
+            exch(x,i,min)
+    def show(self,x):
+        print(x)
+    def exch(self,x,i,j):
+        x[i],x[j]=x[j],x[i]
+    def less(self,a,b):
+        return a<b
+    def __init__(self,comparelist):
+        self.uselist=comparelist
+        choosesort(comparelist)
+        show(comparelist)
+
+x=[1,9,8,5,7,3,4,2]
+y=sortAlgorithm(x)
 
 
 
